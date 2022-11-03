efac8d76914023ff06f1b490f4c7f547b767ef38
GabrielaBezerra <gabrieladecarvalhobezerra@gmail.com>
2022-11-03 14:50:23 -0300

    Initial Commit

---
+++ LearnDebugging/LearnDebuggingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnDebuggingApp.swift
+//  LearnDebugging
+//
+//  Created by Gabriela Bezerra on 03/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LearnDebuggingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
